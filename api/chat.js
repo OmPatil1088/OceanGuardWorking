f46c0f387987680bc442f4782b5169d777c8298f
OmPatil1088 <ompatil1088@gmail.com>
2026-03-11 22:16:13 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -17,7 +17,7 @@ return res.status(400).json({ reply: "Message missing" });
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-model: "gemini-1.5-flash"
+model: "gemini-1.5-flash-latest"
 });
 
 const result = await model.generateContent(message);
